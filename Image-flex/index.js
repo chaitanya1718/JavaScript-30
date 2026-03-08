034fc7d503443a50b4c11edb80b0539c1bc33386
Chaitanya <chaitanyag1718@gmail.com>
2026-03-08 12:29:02 +0530

    image flex box

---
+++ Image-flex/index.js
@@ -0,0 +1,15 @@
+  const panels=document.querySelectorAll('.panel');
+
+        function toggleOpen(){
+            console.log("toggled open");
+            this.classList.toggle('open');
+        }
+        function toggleOpenActive(e){
+            console.log(e.propertyName);
+            if(e.propertyName.includes('flex-grow')){
+                this.classList.toggle('open-active');
+            }
+        }
+
+        panels.forEach(panel=>panel.addEventListener('click',toggleOpen));
+        panels.forEach(panel=>panel.addEventListener('transitionend',toggleOpenActive));
