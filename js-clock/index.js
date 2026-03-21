54279f74bbb0823923f64ec6ffe58169bad0754c
chaitanya <150359158+chaitanya1718@users.noreply.github.com>
2026-03-21 16:20:25 +0530

    Updated js

--- js-clock/index.js
+++ js-clock/index.js
@@ -1,4 +1,5 @@
-  const secondHand = document.querySelector('.sec-hand');
+console.log("started");
+const secondHand = document.querySelector('.sec-hand');
         const minuteHand=document.querySelector('.min-hand');
         const hourHand=document.querySelector('.hour-hand');
         function setDate() {
