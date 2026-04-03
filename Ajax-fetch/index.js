3fdbe9c2f43ff753059c4ee3bb2963ecd9e849f6
chaitanya <150359158+chaitanya1718@users.noreply.github.com>
2026-04-03 23:03:51 +0530

    updated fetch results

--- Ajax-fetch/index.js
+++ Ajax-fetch/index.js
@@ -1,7 +1,7 @@
 
 const api="https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-
+console.log("");
 const cities=[];
 
 async function fetchCities(){
