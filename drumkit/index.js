ce97830e5a35e3cc87da3b15e3b9fbe485669990
chaitanya <150359158+chaitanya1718@users.noreply.github.com>
2026-04-09 23:36:18 +0530

    Worked on redirections

--- drumkit/index.js
+++ drumkit/index.js
@@ -1,5 +1,5 @@
 
-
+console.log("started");
 window.addEventListener("keydown", (e) => {
     const audio = document.querySelector(`audio[data-key="${e.key}"]`);
     const key = document.querySelector(`.key[data-key="${e.key}"]`);
