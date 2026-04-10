3b68828bac72ad1b4e4442d6b7146249a0ac2baf
chaitanya <150359158+chaitanya1718@users.noreply.github.com>
2026-04-10 22:00:22 +0530

    index.js

--- js-clock/index.js
+++ js-clock/index.js
@@ -1,4 +1,4 @@
-console.log("script loaded");
+
 const secondHand = document.querySelector('.sec-hand');
         const minuteHand=document.querySelector('.min-hand');
         const hourHand=document.querySelector('.hour-hand');
